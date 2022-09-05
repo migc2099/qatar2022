9487a61e2c5a522a5b393f63b0c28132d7673458
Miguel <migc.dev@gmail.com>
2022-09-04 18:14:54 -0700

    changed app colors

--- app/src/main/java/com/migc/qatar2022/ui/theme/Color.kt
+++ app/src/main/java/com/migc/qatar2022/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.migc.qatar2022.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val mainColor = Color(0xFF8F1838)
+val backgroundColor = Color(0xFFE6E6DA)
+val groupA = Color(0xFF3E1460)
+val groupB = Color(0xFF82BDB2)
+val groupC = Color(0xFFC63553)
+val groupD = Color(0xFFE7B93B)
+val groupE = Color(0xFF762439)
+val groupF = Color(0xFF63A253)
+val groupG = Color(0xFF4466AD)
+val groupH = Color(0xFFD487B1)
+val knockOutRoundColor = Color(0xFFCB5732)
+val finalsColor = Color(0xFF92795A)
