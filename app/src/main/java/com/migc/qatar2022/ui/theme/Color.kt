6b8dafe6583418778f012fbe25811c545ea3eb61
Miguel <migc.dev@gmail.com>
2022-10-17 22:33:29 -0700

    updated colors

--- app/src/main/java/com/migc/qatar2022/ui/theme/Color.kt
+++ app/src/main/java/com/migc/qatar2022/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.migc.qatar2022.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val mainColor = Color(0xFF8F1838)
+val mainColor = Color(0xFF8D1B3D)
 val mainBackgroundColor = Color(0xFFE6E6DA)
-val goldColor = Color(0xFFFFD700)
-val silverColor = Color(0xFFC0C0C0)
-val bronzeColor = Color(0xFFCD7F32)
+val goldColor = Color(0xFFD6AF36)
+val silverColor = Color(0xFFA7A7AD)
+val bronzeColor = Color(0xFFA77044)
 val rowColor = Color(0xFFEDDDAF)
