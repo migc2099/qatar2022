30852bf75e240a2ea0e18dba037e51ff5c0cae61
Miguel <migc.dev@gmail.com>
2022-10-19 22:04:46 -0700

    added live streaming animation and list placement animation on top teams display

--- app/src/main/java/com/migc/qatar2022/presentation/screens/teams_map/components/TopPredictionsDisplay.kt
+++ app/src/main/java/com/migc/qatar2022/presentation/screens/teams_map/components/TopPredictionsDisplay.kt
@@ -1,12 +1,18 @@
 package com.migc.qatar2022.presentation.components
 
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -24,10 +30,28 @@ import com.migc.qatar2022.domain.model.Predictions
 import com.migc.qatar2022.ui.theme.*
 import kotlin.math.roundToInt
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TopPredictionsDisplay(modifier: Modifier, teams: List<Predictions>) {
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
+
+    LaunchedEffect(key1 = true) {
+
+    }
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val liveAlpha by infiniteTransition.animateFloat(
+        initialValue = 0.4f,
+        targetValue = 0.8f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
 
     Card(
         modifier = modifier
@@ -56,30 +80,59 @@ fun TopPredictionsDisplay(modifier: Modifier, teams: List<Predictions>) {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 item {
-                    Text(
-                        text = stringResource(id = R.string.top_teams_text),
-                        color = mainColor,
-                        fontSize = Typography.caption.fontSize,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-                items(teams.size) { index ->
                     Row(
                         modifier = Modifier
+                            .fillMaxWidth()
+                            .height(TOP_TEAMS_DISPLAY_TITLE_HEIGHT),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Surface(
+                                modifier = Modifier.size(10.dp),
+                                shape = CircleShape,
+                                border = BorderStroke(
+                                    width = 1.dp,
+                                    color = mainColor.copy(0.9f)
+                                ),
+                                content = {}
+                            )
+                            Surface(
+                                modifier = Modifier.size(6.dp),
+                                shape = CircleShape,
+                                color = mainColor.copy(alpha = liveAlpha),
+                                content = {}
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(SMALL_PADDING))
+                        Text(
+                            text = stringResource(id = R.string.top_teams_text),
+                            color = mainColor,
+                            fontSize = Typography.caption.fontSize,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                items(items = teams, key = { it.teamId }) {
+                    Row(
+                        modifier = Modifier
+                            .animateItemPlacement(
+                                animationSpec = tween(durationMillis = 400)
+                            )
                             .fillMaxWidth()
                             .padding(horizontal = SMALL_HORIZONTAL_PADDING),
                         horizontalArrangement = Arrangement.Start,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Image(
-                            painter = painterResource(id = TeamsData.flagsMap[teams[index].teamId]!!),
+                            painter = painterResource(id = TeamsData.flagsMap[it.teamId]!!),
                             contentDescription = stringResource(R.string.home_team_flag),
                             modifier = Modifier.size(FLAG_ROW_IMAGE_SIZE * 0.8f)
                         )
                         Spacer(modifier = Modifier.width(SMALL_HORIZONTAL_PADDING))
                         Text(
                             color = mainColor,
-                            text = stringResource(id = TeamsData.countriesMap[teams[index].teamId]!!),
+                            text = stringResource(id = TeamsData.countriesMap[it.teamId]!!),
                             fontSize = Typography.subtitle2.fontSize
                         )
                     }
