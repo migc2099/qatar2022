9c135ac68f6dc677721535ca3f6a02c9c0c62c45
Miguel <migc.dev@gmail.com>
2022-11-10 13:54:35 -0800

    fixed missing conflict strategy on group table

--- app/src/main/java/com/migc/qatar2022/data/local/dao/GroupDao.kt
+++ app/src/main/java/com/migc/qatar2022/data/local/dao/GroupDao.kt
@@ -2,12 +2,13 @@ package com.migc.qatar2022.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.migc.qatar2022.data.local.entity.GroupEntity
 
 @Dao
 interface GroupDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertGroups(groups: List<GroupEntity>): List<Long>
 
 }
